0d04dac537febcf74a4d003faa825d80ad6f4ce6
mischief <mischief@offblast.org>
2013-02-16 01:50:11 -0800

    add vector.go and remove interpolation code there temporarily

--- cmd/goland/game.go
+++ cmd/goland/game.go
@@ -19,54 +19,6 @@ var (
   logfile = flag.String("log", "goland.log", "log file")
 )
 
-type State struct {
-  X, V float64
-}
-
-type Derivative struct {
-  DX, DV float64
-}
-
-func interpolate(previous, current State, alpha float64) (s State) {
-  s.X = current.X * alpha + previous.X * (1 - alpha)
-  s.V = current.V * alpha + previous.V * (1 - alpha)
-  return
-}
-
-func acceleration(state State, t float64) float64 {
-  k := 10.0
-  b := 1.0
-  return - k*state.X - b*state.V
-}
-
-func evaluate1(initial State, t float64) (der Derivative) {
-  der.DX = initial.V
-  der.DV = acceleration(initial, t)
-  return
-}
-
-func evaluate2(initial State, t, dt float64, d Derivative) (der Derivative) {
-  s := State{X: initial.X + d.DX * dt, V: initial.V + d.DV * dt}
-
-  der.DX = s.V
-  der.DV = acceleration(s, t+dt)
-  return
-}
-
-func integrate(state State, t, dt float64) (s State) {
-  a := evaluate1(state, t)
-  b := evaluate2(state, t, dt * 0.5, a)
-  c := evaluate2(state, t, dt * 0.5, b)
-  d := evaluate2(state, t, dt, c)
-
-  dxdt := 1.0/6.0 * (a.DX + 2.0 * (b.DX + c.DX) + d.DX)
-  dvdt := 1.0/6.0 * (a.DV + 2.0 * (b.DV + c.DV) + d.DV)
-
-  s.X = state.X + dxdt * dt
-  s.V = state.V + dvdt * dt
-  return
-}
-
 type Game struct {
   P           Player
 
@@ -91,13 +43,6 @@ func (g *Game) Run() {
 
   g.Start()
 
-  current := State{X: 20, V: 10}
-  previous := current
-
-  t := 0.0
-  dt := 0.1
-  accumulator := 0.0
-
   timer := NewDeltaTimer()
   ticker := time.NewTicker(time.Second / FPSLimit)
 
@@ -113,22 +58,8 @@ func (g *Game) Run() {
         delta = time.Duration(250 * time.Millisecond)
       }
 
-      accumulator += delta.Seconds()
-
-      for accumulator >= dt {
-        accumulator -= dt
-        previous = current
-        current = integrate(current, t, dt)
-        t += dt
-      }
-
-      state := interpolate(previous, current, accumulator/dt)
-
       g.Update(delta)
       g.Draw()
-
-      g.Print(25+int(state.X), 10, termbox.ColorGreen, termbox.ColorBlack, "@")
-
 
       g.Flush()
 
