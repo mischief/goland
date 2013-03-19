e38a7a9786589118a8f95245f2b0f010fda58996
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-03-18 17:13:00 -0700

    Added Unit.HasItem(*Item)

--- game/unit.go
+++ game/unit.go
@@ -28,6 +28,15 @@ func NewUnit(name string) *Unit {
 	return u
 }
 
+// Checks if a Unit HasItem *Item
+func (u Unit) HasItem (i *Item) bool {
+	if u.Inventory.ContainsItem(i) {
+		return true
+	} 
+	return false
+}
+
+
 func (u Unit) String() string {
 	return fmt.Sprintf("%s: Hp: %d(%d) %s", u.Name, u.Hp, u.HpMax, u.GameObject)
 }
